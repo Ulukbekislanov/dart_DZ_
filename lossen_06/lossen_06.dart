5ddfec7a5205ef183083721339e102264292f2c6
Ulukbekik <islanovulukbek4@gmail.com>
2026-02-24 22:37:44 +0600

    losson 6

--- lossen_06/lossen_06.dart
+++ lossen_06/lossen_06.dart
@@ -4,19 +4,18 @@ void main(){
  Book Potter = Book ('Harry Potter', 'J.K. Rowling');
 
 
- Book sherlock = Book.withRating('Sherlock Holmes', 'Arthur Conan Doyle', 9.3);
+ Book sherlock = Book.withRating('Sherlock Holmes', 'Arthur Conan Doyle', 7.7);
 
 
  Book GameofThrones = Book('Game of Thrones', 'George R.R. Martin');
- 
- GameofThrones.rating = 8.7;
+
+ GameofThrones.rating = 9.5;
  GameofThrones.displayInfo();
 
  Library citylib = Library('Library city');
 citylib.addBook(Potter);   
 citylib.addBook(sherlock); 
 citylib.addBook(GameofThrones);  
- 
 citylib.showBooks();
 print('Total books in library: ${citylib.booksCount()}');
 }
